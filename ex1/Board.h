f3f8e7bc2820ba7f2bc627b1dec53cc650f89555
Asaf Ben Shabat <68661307+BlackChesire@users.noreply.github.com>
2021-03-18 11:20:36 +0200

    Add files via upload

---
+++ ex1/Board.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#ifndef EX1_BOARD_H
+#define EX1_BOARD_H
+using namespace std;
+class Board {
+public:
+    string name;
+    int rows;
+    int cols;
+    char** data;
+    Board(string board_name, int rows, int cols); // creating new board
+    void plot(int x,int y,int color); // ploting the board
+    void line(int x1,int y1,int x2,int y2,int color); // draws a line on the board
+    void circle(int cx,int cy,int radius,int color); // draws a circle on the board
+    void stamp(int x,int y,Board* stamp_board_name); // stamping the board with another board
+    void print(); // printing the board
+    void fill(int x,int y,int color); // filling the board with specfic color
+    int sign(int num); // sign function
+    int check_sign(char c); // checking the color number by the index
+
+};
+#endif //EX1_BOARD_H
