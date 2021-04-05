03cd5cb598fce09ec9e737924accbdf21ff6c880
Asaf Ben Shabat <68661307+BlackChesire@users.noreply.github.com>
2021-04-05 20:23:50 +0300

    Update Board.h

--- ex1/Board.h
+++ ex1/Board.h
@@ -11,7 +11,7 @@ public:
     char** data;
     Board(string board_name, int rows, int cols); // creating new board
     void plot(int x,int y,int color); // ploting the board
-	void plot8(int cx,int cy,int x,int y,int color); // ploting the board 8 times using the plot function
+    void plot8(int cx,int cy,int x,int y,int color); // ploting the board 8 times using the plot function
     void line(int x1,int y1,int x2,int y2,int color); // draws a line on the board
     void circle(int cx,int cy,int radius,int color); // draws a circle on the board
     void stamp(int x,int y,Board* stamp_board_name); // stamping the board with another board
